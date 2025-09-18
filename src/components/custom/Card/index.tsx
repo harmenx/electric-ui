29d94be1e22f079632e892dd67244d921a3657fe
Harmen <harmendark@gmail.com>
2025-09-18 17:31:03 +1200

    feat: add card component and update import path

---
+++ src/components/custom/Card/index.tsx
@@ -0,0 +1,17 @@
+import {
+    Card,
+    CardHeader,
+    CardFooter,
+    CardTitle,
+    CardDescription,
+    CardContent
+} from "@/components/ui/card";
+
+export {
+    Card,
+    CardHeader,
+    CardFooter,
+    CardTitle,
+    CardDescription,
+    CardContent
+};
