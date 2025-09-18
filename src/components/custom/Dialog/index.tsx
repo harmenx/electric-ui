b54ae2e6431772bd5f65f67d66766ac4b214836c
Harmen <harmendark@gmail.com>
2025-09-18 17:45:08 +1200

    feat: add dialog component

---
+++ src/components/custom/Dialog/index.tsx
@@ -0,0 +1,25 @@
+import {
+    Dialog,
+    DialogPortal,
+    DialogOverlay,
+    DialogTrigger,
+    DialogClose,
+    DialogContent,
+    DialogHeader,
+    DialogFooter,
+    DialogTitle,
+    DialogDescription,
+} from "@/components/ui/dialog";
+
+export {
+    Dialog,
+    DialogPortal,
+    DialogOverlay,
+    DialogTrigger,
+    DialogClose,
+    DialogContent,
+    DialogHeader,
+    DialogFooter,
+    DialogTitle,
+    DialogDescription,
+};
