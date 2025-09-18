da26a9a4a2a303664f21567bca750b2515fc028e
Harmen <harmendark@gmail.com>
2025-09-18 17:51:40 +1200

    test: add tests for Dropdown Menu component

---
+++ src/components/custom/DropdownMenu/DropdownMenu.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+} from './index';
+
+describe('Custom DropdownMenu', () => {
+  it('renders a dropdown menu trigger', () => {
+    render(
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+
+    expect(screen.getByText('Open')).toBeInTheDocument();
+  });
+});
